935a8a0d7dd7bbc3c90001f3915eab6d7605baee
Sahil <sahil@hashchainft.com>
2018-12-27 16:45:15 +0530

    added routes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 8000;
 var favicon = require('serve-favicon')
 var path = require('path')
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Home page'))
+app.get('/contact', (req, res) => res.send('contact page'))
+app.get('/about', (req, res) => res.send('about page !'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
