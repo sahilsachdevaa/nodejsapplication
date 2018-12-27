7dbd769e93a3218a967a859b659ccecb5bc191b2
Sahil <sahil@hashchainft.com>
2018-12-27 16:41:56 +0530

    bug fixed

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const app = express()
 const port = process.env.PORT || 8000;
 var favicon = require('serve-favicon')
 var path = require('path')
- 
-var app = express()
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.get('/', (req, res) => res.send('Hello World!'))
 
