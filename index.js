104f0c7d009aa3113fb141c1056f7c594add1220
Sahil <sahil@hashchainft.com>
2018-12-27 16:29:55 +0530

    added new config

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 const port = process.env.port |3000;
-
+var favicon = require('serve-favicon')
+var path = require('path')
+ 
+var app = express()
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
